8295b622eed0349191e544260303879db3fb1405
Paulo Henrique <paulotrg1@gmail.com>
2025-05-01 10:31:27 -0300

    Adição de novas URLs

--- abordagem/views.py
+++ abordagem/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics
+from abordagem.models import Abordagem
+from abordagem.serializers import AbordagemSerializer
+from rest_framework.permissions import IsAuthenticated
+from app.permissions import GlobalDefaultPermission
 
-# Create your views here.
+
+class AbordagemCreateListView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
+    queryset = Abordagem.objects.all()
+    serializer_class = AbordagemSerializer
+
+
+class AbordagemRetrieveUpdateDestoyView(generics.RetrieveDestroyAPIView):
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
+    queryset = Abordagem.objects.all()
+    serializer_class = AbordagemSerializer
