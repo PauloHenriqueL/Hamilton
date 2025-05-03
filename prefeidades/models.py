9dffff83cca4f7f49cee887901aa6592e7d7cceb
Paulo Henrique <paulotrg1@gmail.com>
2025-05-03 14:57:01 -0300

    atualizando prefeidades

--- prefeidades/models.py
+++ prefeidades/models.py
@@ -6,7 +6,7 @@ from terapeuta.models import Terapeuta
 class Prefeidade(models.Model):
     pk_prefeidade = models.AutoField(primary_key=True)
     fk_terapeuta = models.ForeignKey(Terapeuta, on_delete=models.DO_NOTHING, db_column='fk_terapeuta')
-    infantil = models.BooleanField(deafult='false')
+    infantil = models.BooleanField(default=False)
     adolescente = models.BooleanField(deafult='false')
     adulto = models.BooleanField(deafult='false')
     idoso = models.BooleanField(deafult='false')
