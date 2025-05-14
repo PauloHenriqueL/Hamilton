cc3b70603f30233599ac46903752dce59b1d7266
Paulo Henrique <paulotrg1@gmail.com>
2025-05-14 16:50:40 -0300

    desisteciasignal

--- paciente/models.py
+++ paciente/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from captacao.models import Captacao
-from modalidades.models import Modalidade
+from terapeuta.models import Clinica
 
 
 class Paciente(models.Model):
@@ -15,13 +15,19 @@ class Paciente(models.Model):
     email = models.EmailField(blank=True, null=True, verbose_name="E-mail")
     telefone = models.CharField(max_length=20, verbose_name="Telefone do Paciente", help_text="Exemplo: 31988553344 Não coloque +55/espaços/parênteses")
     preferencia = models.CharField(null=True, blank=True, verbose_name="Preferencias",  help_text="Descreva as preferencias do paciente")
-    modalidade = models.CharField(null=True, blank=True, choices=[('Online', 'Online'), ('Presencial', 'Presencial')], verbose_name="Modalidade de atendimento")
+    modalidade = models.CharField(null=True, blank=True, choices=[('Online', 'Online'), ('Presencial', 'Presencial'), ('Híbrido', 'Híbrido')], verbose_name="Modalidade de atendimento")
     contato_apoio = models.CharField(null=True, blank=True, max_length=20, verbose_name="Telefone do Contato de Apoio")
     dat_nascimento = models.DateField(null=True, blank=True, verbose_name="Data de Nascimento")
     vlr_sessao = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Valor da Sessão")
     is_active = models.BooleanField(default=True, verbose_name="Ativo")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Data de Criação")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Data de Atualização")
+    fk_clinica = models.ForeignKey(
+        Clinica, 
+        on_delete=models.CASCADE, 
+        db_column='fk_clinica',
+        verbose_name="Clínica"
+    )
 
     def __str__(self):
         return self.nome
